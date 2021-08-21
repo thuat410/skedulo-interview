903454eec9e00425e604974f1bd5dcf6e202a4cb
Nguyễn Thiện Thuật <thuat410@yahoo.com>
2021-08-22 02:59:36 +0700

    first commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+This function is using for overlapping event in a same time.
+
+This is a nodeJS program so please install nodeJS version >= 12
+
+Go to the function root folder and run:
+1. node run.js <json file>
+2. the result file will be in the same folder with run.js file
+
+Ex: 
+1. node run.js exmaple.json
+2. The result file will be exmaple.optimal.json.expected
---
+++ run.js
@@ -0,0 +1,61 @@
+'use strict';
+const args = process.argv.slice(2);
+const path = './'+args[0];
+let input = require(path);
+const fs = require('fs');
+
+const schedule = () => {
+  if(!input) return [];
+  let data = input.map(i => {
+    i.start = new Date(i.start);
+    i.finish = new Date(i.finish);
+    return i;
+  }).sort((a, b) => b.priority - a.priority);
+  let result = [{
+    band : data[0].band,
+    start : data[0].start,
+    finish : data[0].finish
+  }];
+  for (let i = 1; i < data.length; i++) {
+    result = rebase_result(result, data[i]);
+  }
+  console.log(result.sort((a, b) => a.start - b.start));
+  return result.sort((a, b) => a.start - b.start);
+}
+
+const rebase_result = (result,event) => {
+  const temp_start = getStartDate(result);
+  const temp_finish = getFinishDate(result);
+  const temp_event = {
+    band : event.band,
+    start : event.start,
+    finish : event.finish
+  };
+  if(event.finish < temp_start) return result.concat(temp_event);
+  if(event.start > temp_finish) return result.concat(temp_event);
+  if(event.start < temp_start) {
+    result = result.concat({
+      band : event.band,
+      start : event.start,
+      finish : temp_start
+    })
+  }
+  if(event.finish > temp_finish) {
+    result = result.concat({
+      band : event.band,
+      start : temp_finish,
+      finish : event.finish
+    })
+  }
+  return result;
+}
+
+const getStartDate = (array) => {
+  return array.sort((a, b) => a.start - b.start)[0].start;
+}
+const getFinishDate = (array) => {
+  return array.sort((a, b) => b.finish - a.finish)[0].finish;
+}
+const nameArray = args[0].split(".");
+const newFile = nameArray[0] + '.optimal.json.expected';
+fs.writeFileSync(newFile, JSON.stringify(schedule()));;
